2e16ce4ac9296f7cdeffc272324ab21e59b0f3c4
Shivansh Ahuja <kartikaye02@gmail.com>
2021-08-10 16:25:32 +0530

    Added Jump Mechanics

--- end_runner/Game.cpp
+++ end_runner/Game.cpp
@@ -18,12 +18,18 @@ void Game::initwindow()
 
 void Game::initVariables()
 {
+    this->rectSize.x = 50;
+    this->rectSize.y = 80;
+    this->rectPosY = windowHeight - rectSize.y;
+    this->velocity = 0.0f;
+    this->jumpVelocity = -22.0f;
 
+    this->isGrounded = true;
 }
 
 void Game::update()
 {
-
+    this->playerMovement();
 }
 
 void Game::render()
@@ -32,7 +38,36 @@ void Game::render()
 
     ClearBackground(WHITE);
 
+    this->playerRender();
+
     EndDrawing();
+}
+
+void Game::playerMovement()
+{
+    if (rectPosY >= windowHeight - rectSize.y)
+    {
+        velocity = 0.0f;
+        isGrounded = true;
+    }
+
+    else
+    {
+        velocity += gravity;
+    }
+
+    if (IsKeyPressed(KEY_SPACE) && isGrounded)
+    {
+        velocity += jumpVelocity;
+        isGrounded = false;
+    }
+
+    rectPosY += velocity;
+}
+
+void Game::playerRender()
+{
+    DrawRectangle(windowWidth / 2, rectPosY, rectSize.x, rectSize.y, GREEN);
 }
 
 const bool Game::running() const
--- end_runner/Game.h
+++ end_runner/Game.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include "raylib.h"
+#include <iostream>
 
 class Game
 {
 private:
     const int windowWidth = 512;
     const int windowHeight = 380;
+
+    const int gravity = 1;
+    float jumpVelocity;
+    Vector2 rectSize;
+    int rectPosY;
+    float velocity;
+    bool isGrounded;
 
     void initwindow();
     void initVariables();
@@ -16,6 +24,9 @@ public:
 
     const bool running() const;
     const void close() const;
+
+    void playerMovement();
+    void playerRender();
 
     void update();
     void render();
